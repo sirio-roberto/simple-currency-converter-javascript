db8b25d8cc32d7e6ed39042cf4079497a1243de7
Sirio <sirioroberto1@hotmail.com>
2023-10-04 09:15:25 -0300

    Implemented menu to allow user to select a currency that will be converted to USD

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const input = require("sync-input");
+
 function Currency(name, rateFromUSD) {
   this.name = name;
   this.rateFromUSD = rateFromUSD;
@@ -11,9 +13,54 @@ const currencies = [
   new Currency("GBP", 0.75),
 ]
 
+function getCurrencyNames() {
+  return currencies.map(c => c.name);
+}
+
+function validateCurrencyName(toCurrency) {
+  if (!getCurrencyNames().includes(toCurrency)) {
+    return "Unknown currency";
+  }
+  return "";
+}
+
+function validateCurrencyAmount(amount) {
+  if (isNaN(amount)) {
+    return "The amount has to be a number";
+  }
+  if (amount < 1) {
+    return "The amount cannot be less than 1";
+  }
+  return "";
+}
+
+function convertAmount(fromCurrency, toCurrency, amount) {
+  const rate = currencies.find(c => c.name === toCurrency).rateFromUSD;
+  const result = rate * amount;
+  return result.toFixed(4);
+}
+
 function main() {
   console.log("Welcome to Currency Converter!");
   currencies.forEach(c => console.log(`1 USD equals ${c.rateFromUSD} ${c.name}`));
+  console.log("I can convert USD to these currencies: " + "JPY, EUR, RUB, USD, GBP"); // change it later back to getCurrencyNames
+  let fromCurrency = "USD";
+  console.log("Type the currency you wish to convert: " + fromCurrency);
+
+  let toCurrency = input("To: ").toUpperCase();
+  let errorMsg = validateCurrencyName(toCurrency);
+  if (errorMsg) {
+    console.log(errorMsg);
+  } else {
+    let amount = Number(input("Amount: "));
+    errorMsg = validateCurrencyAmount(amount);
+    if (errorMsg) {
+      console.log(errorMsg);
+    } else {
+      const convertedAmount = convertAmount(fromCurrency, toCurrency, amount);
+      console.log(`Result: ${amount} ${fromCurrency} equals ${convertedAmount} ${toCurrency}`);
+    }
+  }
 }
 
 main();
